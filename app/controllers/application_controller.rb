4e2e09bb30b3ed9471cf5de9d823ae5718f36867
GGG <kuranberitorappu@gmail.com>
2022-05-02 12:23:25 +0000

    Heroku設定追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth if Rails.env.production?
-
-  private
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV['username'] && password == ENV['password']
-    end
-  end
 end
