dc237fd1279efd332011a72092ec07ae5ad522e9
GGG <kuranberitorappu@gmail.com>
2022-05-06 03:02:39 +0000

    ログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_validation { email.downcase! }
   validates :name, presence: true
   validates :email, presence: true
+
   has_secure_password
   has_many :tasks
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions, only: [:new, :create, :destroy]
   resources :tasks
+  resources :users, only:[:new, :create, :show, :edit, :update]
   root to: 'tasks#index'
 end
