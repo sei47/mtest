f0bcafe43bef7f20885478aae7777e226ae1ab0b
GGG <kuranberitorappu@gmail.com>
2022-05-11 14:45:25 +0000

    不具合修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,19 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
 
-  before_update :block_admin_absence
-  before_destroy :block_admin_absence
+  before_update :block_admin_absence_edit
+  before_destroy :block_admin_absence_destroy
   has_secure_password
   has_many :tasks, dependent: :destroy
 
   private
 
-  def block_admin_absence
+  def block_admin_absence_edit
     unless self.admin_flag
       throw(:abort) unless User.where(admin_flag: true).where.not(id: self.id).present?
     end
+  end
+  def block_admin_absence_destroy
+    throw(:abort) unless User.where(admin_flag: true).where.not(id: self.id).present?
   end
 end
