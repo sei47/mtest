7802564ad89deaf95a9de05f0e89f343bca092d0
GGG <kuranberitorappu@gmail.com>
2022-05-11 08:27:55 +0000

    テスト修正

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 # bundle exec rspec spec/system/task_spec.rb
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    @task = FactoryBot.create(:task, title: "test")
+    FactoryBot.create(:second_task, user_id: @task.user_id)
+    visit new_session_path
+    fill_in 'session[email]', with:'123456@email.com'
+    fill_in 'session[password]', with: '4'
+    click_on 'ログイン'
+  end
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -18,7 +26,13 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, title: 'task')
+        visit new_task_path
+        fill_in 'task[title]', with:'task_test'
+        fill_in 'task[content]', with:'task_test'
+        fill_in 'task_deadline', with: '2022/04/13'
+        select '未着手', from: 'task[status]'
+        select '高', from: 'task[priority]'
+        click_on '登録する'
         visit tasks_path
         expect(page).to have_content 'task'
       end
@@ -27,9 +41,8 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクが作成日時の降順に並んでいる場合' do
       it "新しいタスクが一番上に表示される" do
         visit tasks_path
-        task_list = all('.task_row')
-        test_list = Task.all.order(created_at: "DESC")
-        expect(task_list[0]).to eq test_list[-1]
+        task_list = first('.task_row')
+        expect(page).to have_content 'test'
       end
     end
 
@@ -37,17 +50,12 @@ RSpec.describe 'タスク管理機能', type: :system do
       it "期限が降順で表示される" do
         visit tasks_path
         click_on '終了期限'
-        task_list = all('.task_row')
-        test_list = Task.order(deadline: :DESC)
-        expect(task_list[0]).to eq test_list[0]
+        task_list = first('.task_row')
+        expect(page).to have_content '2022-04-18'
       end
     end
   end
   describe '検索機能' do
-    before do
-      FactoryBot.create(:task, title: "test")
-      FactoryBot.create(:second_task)
-    end
     context 'タイトルで曖昧検索した場合' do
       it '検索キーワードを含むタスクで絞り込める' do
         visit tasks_path
@@ -84,8 +92,7 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-         task = FactoryBot.create(:task, title: 'task')
-         visit task_path(task.id)
+         visit task_path(@task.id)
          expect(page).to have_content 'test'
        end
      end
